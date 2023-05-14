083feb8013f726cad810c5e8be0a0d110f12d1af
sudachi0114 <jnevariste@gmail.com>
2023-05-14 16:10:55 +0900

    gevent StreamServer が localhost:16379 で待ち受けるようになった

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from gevent.server import StreamServer
+
 class Server(object):
     def __init__(self) -> None:
-        pass
+        self._server = StreamServer(("127.0.0.1", 16379), self.handle)
+
+    def handle(self, socket, address):
+        print("new connection!")
 
     def run(self):
-        print("Hello world!")
+        self._server.serve_forever()
 
 
 if __name__ == "__main__":
