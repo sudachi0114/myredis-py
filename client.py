992aeb6917f998948c335b414be6899955217014
sudachi0114 <jnevariste@gmail.com>
2023-05-20 01:01:51 +0900

    ソケットに接続してコマンドを送信するように変更

--- client.py
+++ client.py
@@ -1,9 +1,16 @@
+from gevent import socket
+
 class Client(object):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, host: str = "127.0.0.1", port: int = 16379) -> None:
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((host, port))
+        self._fh = self._socket.makefile("rwb")
 
     def execute(self, *args):
-        print(args)
+        command = " ".join(args).encode("utf-8")
+        print(command)
+        self._fh.write(command)
+        self._fh.flush()
 
     def set(self, key, value):
         return self.execute("SET", key, value)
