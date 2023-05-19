ad1f4fad0d2c1504931c8adc69529a85b18bffec
sudachi0114 <jnevariste@gmail.com>
2023-05-20 01:42:15 +0900

    Redis の GET と SET, レスポンスを行うファンクションを実装

--- main.py
+++ main.py
@@ -11,15 +11,37 @@ class Server(object):
             (host, port), self.connection_handler, spawn=self._pool
         )
 
+        self._kv = {}
+        self._commands = self.get_commands()
+
     def connection_handler(self, socket, address):
         socket_file = socket.makefile("rwb")
-        self.get_response(socket_file.readline())
+        raw_request = socket_file.readline().rstrip(b'\r\n')
+        print(raw_request)
+        request = raw_request.decode('utf-8')
+        response = self.get_response(request)
+        print(response)
 
     def get_commands(self):
-        return {}
+        return {
+            "GET": self.get,
+            "SET": self.set
+        }
 
     def get_response(self, request):
+        # TODO: validation
+        request = request.split()
         print(request)
+        command = request[0].upper()
+        print(command)
+        return self._commands[command](*request[1:])
+
+    def set(self, key, value):
+        self._kv[key] = value
+        return 1
+
+    def get(self, key):
+        return self._kv.get(key)
 
     def run(self):
         self._server.serve_forever()
